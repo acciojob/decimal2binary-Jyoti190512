bd54f18d46f042dbc7351adafedea3673742cde7
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-07-15 00:31:56 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function decimalToBinary(num) {
   //Write you code here
+	return num.toString(2)
   
 }
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
 
-window.decimalToBinary = decimalToBinary;
+//window.decimalToBinary = decimalToBinary;//
